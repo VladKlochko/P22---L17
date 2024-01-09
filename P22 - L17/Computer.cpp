6916d296500f7ebbe8060324c4cdf368cb4471df
Vladik <vlad53830@gmail.com>
2024-01-09 17:58:47 +0200

    Add main.cpp, Computer.cpp

--- P22 - L17/Computer.cpp
+++ P22 - L17/Computer.cpp
@@ -1,1 +1,24 @@
 #include "Computer.h"
+
+Computer::Computer(string name, float frequency, int ram, bool dvd, float cost)
+{
+    this->name = name;
+    this->frequency = frequency;
+    this->ram = ram;
+    this->dvd = dvd;
+    this->cost = cost;
+}
+
+int Computer::getRam() const
+{
+    return ram;
+}
+
+void Computer::show() const
+{
+    cout << "Name: " << name << endl
+        << "Frequency: " << frequency << "GHz" << endl
+        << "Ram: " << ram << "GB" << endl
+        << "DVD: " << dvd << endl
+        << "Cost: " << cost << "UAH." << endl;
+}
---
+++ P22 - L17/main.cpp
@@ -0,0 +1,17 @@
+#include "Computer.h"
+#include <vector>
+
+int main()
+{
+	vector<Computer> arr;
+	arr.push_back(Computer("HP", 1.2, 4, 0, 20000));
+	arr.push_back(Computer("Acer", 3.2, 16, 0, 35000));
+	arr.push_back(Computer("MSI", 6.4, 32, 0, 64000));
+	arr.push_back(Computer("HP", 3.2, 8, 1, 32000));
+
+	for (int i = 0; i < arr.size(); i++)
+	{
+		arr[i].show();
+		cout << endl;
+	}
+}
